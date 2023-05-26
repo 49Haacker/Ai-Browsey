cab64fa015b1c768ac166a10bb15360d751fe0a9
Gaurav <gauravtiwari7888@gmail.com>
2023-05-26 12:34:50 +0530

    home page

--- front-end/src/component/Main/Home.js
+++ front-end/src/component/Main/Home.js
@@ -11,6 +11,41 @@ import React from "react";
 const Home = () => {
   return (
     <>
+      <div className="container d-flex mt-3">
+        <div>
+          <h1>AI Browsey</h1>
+          <h2 className="small">Train a computer to recognize your own images, sounds, & poses.</h2>
+          <p>A fast, easy way to create machine learning models for your sites, apps, and more no expertise or coding required.</p>
+          <a href="/">
+            <button type="submit">Get Started</button>
+          </a>
+          <div className="d-flex">
+            <a href="https://www.tensorflow.org/js" className="mt-3">
+              <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/tensorflow_logo_icon_168671.png" alt="TensorFlow" className="w-100" style={{ height: '3rem' }} />
+            </a>
+            <a href="https://www.tensorflow.org/js" className="mt-3">
+              <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/tensorflow_logo_icon_168671.png" alt="TensorFlow" className="w-100" style={{ height: '3rem' }} />
+            </a>
+            <a href="https://www.tensorflow.org/js" className="mt-3">
+              <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/tensorflow_logo_icon_168671.png" alt="TensorFlow" className="w-100" style={{ height: '3rem' }} />
+            </a>
+            <a href="https://www.tensorflow.org/js" className="mt-3">
+              <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/tensorflow_logo_icon_168671.png" alt="TensorFlow" className="w-100" style={{ height: '3rem' }} />
+            </a>
+            <a href="https://www.tensorflow.org/js" className="mt-3">
+              <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/tensorflow_logo_icon_168671.png" alt="TensorFlow" className="w-100" style={{ height: '3rem' }} />
+            </a>
+            <a href="https://www.tensorflow.org/js" className="mt-3">
+              <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/tensorflow_logo_icon_168671.png" alt="TensorFlow" className="w-100" style={{ height: '3rem' }} />
+            </a>
+          </div>
+        </div>
+        <div className="d-flex w-100" style={{ height: '15rem' }}>
+          <video class="w-100" autoplay loop muted>
+            <source src="https://mdbootstrap.com/img/video/animation-intro.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
     </>
   )
 }
