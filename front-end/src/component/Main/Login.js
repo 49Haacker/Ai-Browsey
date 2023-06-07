4b4f9e30c9d7657103d231af12304046905cd5df
m-triple-m <triplem656@gmail.com>
2023-06-07 19:54:06 +0530

    added image upload

--- front-end/src/component/Main/Login.js
+++ front-end/src/component/Main/Login.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import './login.css' 
+import React from 'react';
+import './login.css';
+import {useFormik} from 'formik';
 
 const Login = () => {
 
- 
+  const loginForm = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: (value) => {
+      console.log(value);
+    }
+  });
+
   return (
     <>
       <section className="vh-100">
         <div className="container-fluid h-custom">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-md-9 col-lg-6 col-xl-5">
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                className="img-fluid" alt="Sampleimage" />
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid" alt="Sampleimage" />
             </div>
             <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-              <form>
+              <form onSubmit={loginForm.handleSubmit}>
                 <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
                   <p className="lead fw-normal mb-0 me-3">Sign in with</p>
                   <button type="button" className="btn btn-primary btn-floating mx-1 custom-btn">
@@ -35,17 +44,19 @@ const Login = () => {
                 </div>
 
                 {/* <!-- Email input --> */}
-                <div className="form-outline mb-4">
-                  <input type="email" id="form3Example3" className="form-control form-control-lg"
-                    placeholder="Enter a valid email address" />
-                  <label className="form-label" for="form3Example3">Email address</label>
+                <div className=" mb-4">
+                  <label className="form-label" for="form3Example3">
+                    Email address
+                  </label>
+                  <input type="email" id="email" onChange={loginForm.handleChange} value={loginForm.values.email} className="form-control form-control-lg" placeholder="Enter a valid email address" />
                 </div>
 
                 {/* <!-- Password input --> */}
-                <div className="form-outline mb-3">
-                  <input type="password" id="form3Example4" className="form-control form-control-lg"
-                    placeholder="Enter password" />
-                  <label className="form-label" for="form3Example4">Password</label>
+                <div className=" mb-3">
+                  <label className="form-label" for="form3Example4">
+                    Password
+                  </label>
+                  <input type="password" id="password" onChange={loginForm.handleChange} value={loginForm.values.password} className="form-control form-control-lg" placeholder="Enter password" />
                 </div>
 
                 <div className="d-flex justify-content-between align-items-center">
@@ -56,23 +67,29 @@ const Login = () => {
                       Remember me
                     </label>
                   </div>
-                  <a href="#!" className="text-body">Forgot password?</a>
+                  <a href="#!" className="text-body">
+                    Forgot password?
+                  </a>
                 </div>
 
                 <div className="text-center text-lg-start mt-4 pt-2">
-                  <button type="button" className="btn btn-primary btn-lg"
-                    style={{ paddingLeft: '2.5rem; padding-right: 2.5rem' }}>Login</button>
-                  <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                    className="link-danger">Register</a></p>
+                  <button type="submit" className="btn btn-primary btn-lg" style={{ paddingLeft: '2.5rem; padding-right: 2.5rem' }}>
+                    Login
+                  </button>
+                  <p className="small fw-bold mt-2 pt-1 mb-0">
+                    Don't have an account?{' '}
+                    <a href="#!" className="link-danger">
+                      Register
+                    </a>
+                  </p>
                 </div>
-
               </form>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- front-end/src/component/User/ImageModelTrainer.js
+++ front-end/src/component/User/ImageModelTrainer.js
@@ -1,95 +1,88 @@
-import React, { useEffect, useState } from "react";
-import * as tf from "@tensorflow/tfjs";
-import "@tensorflow/tfjs";
+import React, { useEffect, useState } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs';
 // import '@tensorflow/tfjs-node';
-import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as mobilenet from '@tensorflow-models/mobilenet';
 import { categoricalCrossentropy } from '@tensorflow/tfjs-layers/dist/exports_metrics';
 
-const imageList1 = [
-  'apple-1.jpg',
-  'apple-2.jpg',
-  'apple-3.jpg',
-  'apple-4.jpg',
-  'apple-5.jpg',
-  'orange-1.jpg',
-  'orange-2.jpg',
-  'orange-3.jpg',
-  'orange-4.jpg',
-  'orange-5.jpg',
-]
+const imageList1 = ['apple-1.jpg', 'apple-2.jpg', 'apple-3.jpg', 'apple-4.jpg', 'apple-5.jpg', 'orange-1.jpg', 'orange-2.jpg', 'orange-3.jpg', 'orange-4.jpg', 'orange-5.jpg'];
 
 const label_x1 = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0];
 const label_x2 = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
 
-const imageList2 = [
-  'orange-1.jpg',
-  'orange-2.jpg',
-  'orange-3.jpg',
-  'orange-4.jpg',
-  'orange-5.jpg',
-]
+const imageList2 = ['orange-1.jpg', 'orange-2.jpg', 'orange-3.jpg', 'orange-4.jpg', 'orange-5.jpg'];
 
 const classes = ['apple', 'apple', 'apple', 'apple', 'apple', 'orange', 'orange', 'orange', 'orange', 'orange'];
 
-const ImageModelTrainer = () => {
 
+const ImageModelTrainer = () => {
   const [batchSize, setBatchSize] = useState(5);
+  const [selImages, setSelImages] = useState([]);
+
+  const loadClientImages = (e) => {
+    const files = e.target.files;
+    console.log(files);
+    if (files && files.length) {
+      for(let file in files){
+        if (file) {
+          const reader = new FileReader();
+      
+          reader.onload = (e) => {
+            const imageDataUrl = e.target.result;
+            setSelImages([...selImages, imageDataUrl]);
+            console.log(imageDataUrl);
+            // Do something with the image data URL (e.g., display it, process it, etc.)
+          };
+      
+          reader.readAsDataURL(file);
+        }
+      }
+    
+  }
+  }
 
   const train = async () => {
     const { images, targets } = generateData();
     // this.ProgressBarValue=35;
-    console.log("Images are loaded into the memory as tensor !", "Close");
+    console.log('Images are loaded into the memory as tensor !', 'Close');
 
     const mobilenetModified = await getModifiedMobilenet();
     // this.ProgressBarValue=50;
-    console.log("Modefiled Mobilenet AI Model is loaded !", "Close");
+    console.log('Modefiled Mobilenet AI Model is loaded !', 'Close');
 
     await fineTuneModifiedModel(mobilenetModified, images, targets);
-    console.log("Model training is completed !", "Close");
+    console.log('Model training is completed !', 'Close');
     // this.ProgressBarValue=100;
-  }
+  };
 
   const getModifiedMobilenet = async () => {
-    const trainableLayers = [
-      "denseModified",
-      "conv_pw_13_bn",
-      "conv_pw_13",
-      "conv_dw_13_bn",
-      "conv_dw_13",
-    ];
-    const mobilenet = await tf.loadLayersModel(
-      "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json"
-    );
-    console.log("Mobilenet model is loaded");
+    const trainableLayers = ['denseModified', 'conv_pw_13_bn', 'conv_pw_13', 'conv_dw_13_bn', 'conv_dw_13'];
+    const mobilenet = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+    console.log('Mobilenet model is loaded');
 
-    const x = mobilenet.getLayer("global_average_pooling2d_1");
-    const predictions = tf.layers
-      .dense({ units: 2, activation: "softmax", name: "denseModified" })
-      .apply(x.output);
+    const x = mobilenet.getLayer('global_average_pooling2d_1');
+    const predictions = tf.layers.dense({ units: 2, activation: 'softmax', name: 'denseModified' }).apply(x.output);
     let mobilenetModified = tf.model({
       inputs: mobilenet.input,
       outputs: predictions,
-      name: "modelModified",
+      name: 'modelModified'
     });
-    console.log("Mobilenet model is modified");
+    console.log('Mobilenet model is modified');
 
-    mobilenetModified = freezeModelLayers(
-      trainableLayers,
-      mobilenetModified
-    );
-    console.log("ModifiedMobilenet model layers are freezed");
+    mobilenetModified = freezeModelLayers(trainableLayers, mobilenetModified);
+    console.log('ModifiedMobilenet model layers are freezed');
 
     mobilenetModified.compile({
       loss: categoricalCrossentropy,
       optimizer: tf.train.adam(1e-3),
-      metrics: ["accuracy", "crossentropy"],
+      metrics: ['accuracy', 'crossentropy']
     });
 
     return mobilenetModified;
   };
 
   const loadImage = () => {
-    const imgElement = document.getElementById("my-image");
+    const imgElement = document.getElementById('my-image');
 
     // Create the image tensor
     const imageTensor = tf.browser.fromPixels(imgElement);
@@ -98,7 +91,6 @@ const ImageModelTrainer = () => {
     console.log(imageTensor.shape);
     return imageTensor;
   };
-
 
   const freezeModelLayers = (trainableLayers, mobilenetModified) => {
     for (const layer of mobilenetModified.layers) {
@@ -111,7 +103,7 @@ const ImageModelTrainer = () => {
       }
     }
     return mobilenetModified;
-  }
+  };
 
   const fineTuneModifiedModel = async (model, images, targets) => {
     function onBatchEnd(batch, logs) {
@@ -121,14 +113,14 @@ const ImageModelTrainer = () => {
     }
     console.log('Finetuning the model...');
 
-    await model.fit(images, targets,
-      {
+    await model
+      .fit(images, targets, {
         epochs: 5,
         batchSize: 24,
         validationSplit: 0.2,
         callbacks: { onBatchEnd }
-
-      }).then(info => {
+      })
+      .then((info) => {
         // console.log
         console.log('Final accuracy', info.history.acc);
         console.log('Cross entropy', info.ce);
@@ -145,14 +137,13 @@ const ImageModelTrainer = () => {
 
           // this.traningMetrics[k].acc=info.history['acc'][k];
           // this.traningMetrics[k].ce=info.history['ce'][k];
-          // this.traningMetrics[k].loss=info.history['loss'][k]; 
+          // this.traningMetrics[k].loss=info.history['loss'][k];
         }
         images.dispose();
         targets.dispose();
         model.dispose();
-      });;
-
-  }
+      });
+  };
 
   function parseImages(batchSize) {
     if (this.isImagesListed) {
@@ -171,28 +162,26 @@ const ImageModelTrainer = () => {
 
       this.tableRows.push({ ImageSrc: '', LabelX1: 0, LabelX2: 0, Class: '' });
 
-      if (cols[0].split(csvSeparator_2)[1] == "png") {
-
-        if (cols[1] == "Uninfected") {
+      if (cols[0].split(csvSeparator_2)[1] == 'png') {
+        if (cols[1] == 'Uninfected') {
           this.label_x1.push(Number('1'));
           this.label_x2.push(Number('0'));
 
-          this.tableRows[i].ImageSrc = "../assets/" + cols[0];
+          this.tableRows[i].ImageSrc = '../assets/' + cols[0];
           this.tableRows[i].LabelX1 = 1;
           this.tableRows[i].LabelX2 = 0;
-          this.tableRows[i].Class = "Uninfected";
+          this.tableRows[i].Class = 'Uninfected';
         }
 
-        if (cols[1] == "Parasitized") {
+        if (cols[1] == 'Parasitized') {
           this.label_x1.push(Number('0'));
           this.label_x2.push(Number('1'));
 
-          this.tableRows[i].ImageSrc = "../assets/" + cols[0];
+          this.tableRows[i].ImageSrc = '../assets/' + cols[0];
           this.tableRows[i].LabelX1 = 0;
           this.tableRows[i].LabelX2 = 1;
-          this.tableRows[i].Class = "Parasitized";
+          this.tableRows[i].Class = 'Parasitized';
         }
-
       }
     }
     this.table.renderRows();
@@ -227,13 +216,13 @@ const ImageModelTrainer = () => {
 
     return { images, targets };
 
-    // for ( let i = 0; i < batchSize; i++) 
+    // for ( let i = 0; i < batchSize; i++)
     // {
     //   // split content based on comma
     //   const cols = allTextLines[i].split(csvSeparator);
     //   console.log(cols[0].split(csvSeparator_2)[0])
 
-    //   if (cols[0].split(csvSeparator_2)[1]=="png") 
+    //   if (cols[0].split(csvSeparator_2)[1]=="png")
     //   {
     //     console.log(i)
     //     const imageTensor = this.capture(i);
@@ -244,10 +233,10 @@ const ImageModelTrainer = () => {
     //     targetTensors.push(targetTensor);
 
     //     imageTensor.print(true);
-    //   } 
+    //   }
     // }
     // const images = tf.stack(imageTensors);
-    // const targets = tf.stack(targetTensors);   
+    // const targets = tf.stack(targetTensors);
 
     // return {images, targets};
   }
@@ -269,7 +258,6 @@ const ImageModelTrainer = () => {
     this.csvContent = textFromFileLoaded;
   }
 
-
   function onFileSelect(input) {
     const files = input.files;
 
@@ -280,30 +268,28 @@ const ImageModelTrainer = () => {
       fileReader.onload = (event) => {
         const textFromFileLoaded = fileReader.result;
         this.csvContent = textFromFileLoaded;
-      }
+      };
 
-      fileReader.readAsText(fileToRead, "UTF-8");
+      fileReader.readAsText(fileToRead, 'UTF-8');
 
-      console.log("Filename: " + files[0].name);
-      console.log("Type: " + files[0].type);
-      console.log("Size: " + files[0].size + " bytes");
+      console.log('Filename: ' + files[0].name);
+      console.log('Type: ' + files[0].type);
+      console.log('Size: ' + files[0].size + ' bytes');
     }
   }
 
   function getTotalUninfected() {
-    return this.tableRows.map(t => t.LabelX1).reduce((acc, value) => acc + value, 0);
-  };
-
+    return this.tableRows.map((t) => t.LabelX1).reduce((acc, value) => acc + value, 0);
+  }
 
   function getTotalPAratisized() {
-    return this.tableRows.map(t => t.LabelX2).reduce((acc, value) => acc + value, 0);
-  };
-
+    return this.tableRows.map((t) => t.LabelX2).reduce((acc, value) => acc + value, 0);
+  }
 
   useEffect(() => {
     // initClassifier();
     // getModifiedMobilenet();
-    train();
+    // train();
   }, []);
 
   const predict = async (image) => {
@@ -318,14 +304,11 @@ const ImageModelTrainer = () => {
     // // Define the model architecture
     // const model = tf.sequential();
     // model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
-
     // // Compile the model
     // model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
-
     // // Generate some training data
     // const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
     // const ys = tf.tensor2d([2, 4, 6, 8], [4, 1]);
-
     // // Train the model
     // model.fit(xs, ys, { epochs: 10 }).then(() => {
     //   // Use the model to make predictions
@@ -337,13 +320,13 @@ const ImageModelTrainer = () => {
   const uploadFile = (e) => {
     const file = e.target.files[0];
     const fd = new FormData();
-    fd.append("myfile", file);
-    fetch(URL + "/util/uploadfile", {
-      method: "POST",
-      body: fd,
+    fd.append('myfile', file);
+    fetch(URL + '/util/uploadfile', {
+      method: 'POST',
+      body: fd
     }).then((res) => {
       if (res.status === 200) {
-        console.log("file uploaded");
+        console.log('file uploaded');
       }
     });
   };
@@ -351,7 +334,7 @@ const ImageModelTrainer = () => {
   return (
     <>
       <div className="row">
-        <div className="col-md-1">
+        {/* <div className="col-md-1">
           {
             imageList1.map((image, index) => (
               <img
@@ -372,146 +355,66 @@ const ImageModelTrainer = () => {
               />
             ))
           }
-        </div>
+        </div> */}
       </div>
       <div className="d-flex mt-5">
         <div className="row w-100 m-0">
-          <div className="col-md-4">
-            <div
-              className="pb-3 text-center"
-              style={{ backgroundColor: "blue" }}
-            >
-              <h2>Upload Image</h2>
-            </div>
-            <div className="pb-3 d-flex justify-content-center mt-2 w-100">
-              <label for="formFileMultiple" className="form-label">
-                Multiple files input example
-              </label>
-              <input
-                className="form-control"
-                type="file"
-                id="formFileMultiple"
-                multiple
-              />
-            </div>
-
-            <div className="row">
-              <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                  className="w-100 shadow-1-strong rounded mb-4"
-                  alt="Boat on Calm Water"
-                />
-
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
-                  className="w-100 shadow-1-strong rounded mb-4"
-                  alt="Wintry Mountain Landscape"
-                />
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-header">
+                <h3>Selected Image Class</h3>
               </div>
-
-              <div className="col-lg-4 mb-4 mb-lg-0">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
-                  className="w-100 shadow-1-strong rounded mb-4"
-                  alt="Mountains in the Clouds"
-                />
-
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                  className="w-100 shadow-1-strong rounded mb-4"
-                  alt="Boat on Calm Water"
-                />
-              </div>
-
-              <div className="col-lg-4 mb-4 mb-lg-0">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-                  className="w-100 shadow-1-strong rounded mb-4"
-                  alt="Waves at Sea"
-                />
-
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
-                  className="w-100 shadow-1-strong rounded mb-4"
-                  alt="Yosemite National Park"
-                />
+              <div className="card-body" style={{ height: '90vh', overflow: 'auto' }}>
+                <label>Select Images To Train</label>
+                <input type="file" className="form-control" multiple onChange={loadClientImages} />
+                <div className="row mt-4">
+                  <div className="col-md-6">
+                    {imageList1.map((image, index) => (
+                      <img src={`/traningImages/${image}`} id={image} width="224" height="224" />
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="col-md-8">
-            <div
-              className="col-lg-4 mb-4 mb-lg-0 text-center w-100 pb-3"
-              style={{ backgroundColor: "red" }}
-            >
+          <div className="col-md-9">
+            <div className="col-lg-4 mb-4 mb-lg-0 text-center w-100 pb-3" style={{ backgroundColor: 'red' }}>
               <h2>class</h2>
             </div>
             <div className="row row-cols-4 g-3 pt-2">
               <div className="col">
                 <div className="card">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
-                    className="card-img-top"
-                    alt="Hollywood Sign on The Hill"
-                  />
+                  <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" className="card-img-top" alt="Hollywood Sign on The Hill" />
                   <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      This is a longer card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
+                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                   </div>
                 </div>
               </div>
               <div className="col">
                 <div className="card">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp"
-                    className="card-img-top"
-                    alt="Palm Springs Road"
-                  />
+                  <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" className="card-img-top" alt="Palm Springs Road" />
                   <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      This is a longer card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
+                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                   </div>
                 </div>
               </div>
               <div className="col">
                 <div className="card">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp"
-                    className="card-img-top"
-                    alt="Palm Springs Road"
-                  />
+                  <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" className="card-img-top" alt="Palm Springs Road" />
                   <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      This is a longer card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
+                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                   </div>
                 </div>
               </div>
               <div className="card">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/new/standard/city/050.webp"
-                  className="card-img-top"
-                  alt="Skyscrapers"
-                />
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/050.webp" className="card-img-top" alt="Skyscrapers" />
                 <div className="card-body">
                   <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </p>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
               </div>
             </div>
